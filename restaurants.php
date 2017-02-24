a865fd4c93b103b15c71fd27b2cf6b875c588489
Mahmoud Enani <m.a.enani21@gmail.com>
2017-02-23 23:44:25 -0500

    all completed - for final testing on server

--- restaurants.php
+++ restaurants.php
@@ -26,29 +26,24 @@ class Restaurants {
     public function getByType(string $type, $budget, string $optInOut, string $waitTime) {
         $filteredRestaurants = [];
         foreach($this->restaurants as $restaurant) {
-
-          // return all list if default options are selected
-          // if ($type == 'any' && $budget == 'any' && $optInOut && $waitTime == ''){
-          //   $filteredRestaurants[] = $restaurant;
-          // }
-          if ($type == 'any' && $restaurant['price'] == $budget && $restaurant['dining'] == $optInOut && ($restaurant['wait_time']  == 0 || $restaurant['wait_time'] <= $waitTime)){
-            $filteredRestaurants[] = $restaurant;
+            // filter if no cuisine type is selected
+            if ($type == 'any' && $restaurant['price'] == $budget && $restaurant['dining'] == $optInOut && ($restaurant['wait_time']  == 0 || $restaurant['wait_time'] <= $waitTime)){
+              $filteredRestaurants[] = $restaurant;
+            }
+            // filter on all forms
+            else if ($restaurant['type'] == $type  && $restaurant['price'] == $budget && $restaurant['dining'] == $optInOut && ($restaurant['wait_time']  == 0 || $restaurant['wait_time'] <= $waitTime)){
+              $filteredRestaurants[] = $restaurant;
+            }
           }
-          else if ($restaurant['type'] == $type  && $restaurant['price'] == $budget && $restaurant['dining'] == $optInOut && ($restaurant['wait_time']  == 0 || $restaurant['wait_time'] <= $waitTime)){
-            $filteredRestaurants[] = $restaurant;
+          // sort the array by restaurant name
+          function sortResturants($a,$b)
+          {
+            if ($a['name']==$b['name']) return 0;
+            return ($a['name']<$b['name'])?-1:1;
           }
 
+          usort($filteredRestaurants, "sortResturants");
+
+          return $filteredRestaurants;
         }
-
-        // sort the array by restaurant name
-        function sortResturants($a,$b)
-        {
-        if ($a['name']==$b['name']) return 0;
-        return ($a['name']<$b['name'])?-1:1;
-        }
-
-        usort($filteredRestaurants, "sortResturants");
-
-        return $filteredRestaurants;
-    }
 } # end of class
